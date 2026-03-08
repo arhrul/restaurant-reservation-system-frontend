58f65781f8a983f7f07597d95e5e7e3d43916ca8
arhrul <arhrul@taltech.ee>
2026-03-08 17:05:12 +0200

    Add a reserved date

--- src/app/pages/reservation.page/reservation.page.ts
+++ src/app/pages/reservation.page/reservation.page.ts
@@ -131,4 +131,14 @@ export class ReservationPage implements OnInit {
     this.reservationSuccess = true;
   }
 
+  getFormattedDate(date: Date): string {
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
+
 }
